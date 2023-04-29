ee27414ee9165f4fbc5813ae03877f607d76f817
DipalDonga <128701888+DipalDonga@users.noreply.github.com>
2023-04-29 21:57:59 +0530

    Update login_version2.php

--- login_version2.php
+++ login_version2.php
@@ -41,9 +41,7 @@ if(isset($_SESSION['mjwadmin_id']))
                 <h3><center>Login Form</center></h3>
                                 </div>
 
-                                
-
-                    
+                   
             <form class="form-horizontal" id="loginForm" role="form" >
                     <label class="" for="username">Username:</label>
                 <div class="form-group">

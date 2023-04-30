7e88dd82e35d706ceebcd53c93245d5b0442a02e
DipalDonga <128701888+DipalDonga@users.noreply.github.com>
2023-05-01 02:56:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -73,13 +73,12 @@ include("include/connect.php");
 					<li><a href="<?php echo $web; ?>contact.php">Contact</a></li>
 					<li><a href="<?php echo $web; ?>login.php">Login</a></li>
 					<li class=""><input type="text" class="newsinput" placeholder="Enter e-mail adress">
-                    <button class="newsbtn">Search</button></li>
+                   				 <button class="newsbtn">Search</button></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
 					<li class="search_dropdown"><a href="#"><i class="fa fa-call"></i></a></li>
 				</ul>
 			</div>
-
 			</div>
 				</div>
 			</div>
@@ -91,9 +90,7 @@ include("include/connect.php");
 		<div class="slider_bg owl-carousel">
 		<div class="item">
 			<img src="http://localhost/secondhand_shop/onhome-slider/books-12935969.jpg" alt="">
-		</div>
-						
-				
+		</div>	
 		</div>
 	</div>
 </section>
@@ -102,9 +99,9 @@ include("include/connect.php");
 	<section class="about-area"  id="aboutus">
 
 		<div class="about-inner row m0">
-						<div class="about-title">
-							<h2 id="aus">About Secondhand Shop</h2>
-						</div>
+			<div class="about-title">
+				<h2 id="aus">About Secondhand Shop</h2>
+			</div>
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 about-content">
@@ -172,5 +169,4 @@ include("include/connect.php");
 <?php include("include/footer.php"); ?>
 
 </body>
-
 </html>

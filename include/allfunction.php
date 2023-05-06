36b27483648a4ce933764f6ecc4c4befc2e7ba90
21-ri <131352850+21-ri@users.noreply.github.com>
2023-05-06 17:42:27 +0530

    Add files via upload

--- include/allfunction.php
+++ include/allfunction.php
@@ -1,10 +1,23 @@
 <?php
+session_start();
 include("connect.php");
 
 date_default_timezone_set('Asia/Kolkata');
 
 switch($_REQUEST['action'])
 {
+	case "signin":
+		login();
+		break;
+	case "signout":
+		logout();
+		break;
+	case "signup":
+		signup();
+		break;
+	case "addproduct":
+		addproduct();
+		break;
 	case "product_listing":
 		product_listing();
 		break;
@@ -25,6 +38,221 @@ function fun404()
 {
 header('http://mtj.world/wrong-galaxy-404.html');
 }
+
+function login(){
+	global $link;
+	global $web;
+	global $web1;
+	
+
+	date_default_timezone_set('Europe/London');
+	$sTime = date("d-m-Y H:i:s");  
+
+	$escapedName = mysqli_real_escape_string($link,$_POST['username']);
+	$escapedPW = md5(mysqli_real_escape_string($link,$_POST['password']));
+	//query execute
+
+	$query = mysqli_query($link,"select * from student_info where semail ='".$escapedName."' and password='".$escapedPW."'");
+	
+	//check if data null or not
+
+	if(mysqli_num_rows($query) > 0){
+		
+		$row = mysqli_fetch_array($query);
+
+		$_SESSION['mjwadmin_id']=$row['studid'];
+		$_SESSION['mjwadmin_name']=$row['sname'];
+
+		$query1 = mysqli_query($link,"update student_info set `semail`='".$escapedName."',`log_in`='".$sTime."' where studuni_id='".$row['studuni_id']."'");
+
+		echo "1";
+	}
+	else{
+		echo "0";
+	}
+}
+function logout(){
+	global $web;
+
+	$id=$_SESSION['mjwadmin_id'];
+	if(isset($_SESSION['mjwadmin_id']))
+	{
+		session_unset(); 
+		session_destroy();
+		header('location:'.$web);
+	}
+	else
+		header('location:'.$web);
+
+}
+
+function signup(){
+	global $link;
+	global $web;
+	global $web1;
+		
+	if(isset($_REQUEST['studid'])){
+		$studid=$_REQUEST['studid'];
+	}else{
+		$studid='';
+	}
+	if(isset($_REQUEST['sname'])){
+		$sname=$_REQUEST['sname'];
+	}else{
+		$sname='';
+	}
+	if(isset($_REQUEST['semail'])){
+		$semail=$_REQUEST['semail'];
+	}
+	else{
+		$semail='';
+	}
+
+	if(isset($_REQUEST['contact'])){
+		$contact=$_REQUEST['contact'];
+	}
+	else{
+		$contact='';
+	}
+
+	if(isset($_REQUEST['password'])){
+		$password = md5(mysqli_real_escape_string($link,$_REQUEST['password']));
+	}
+	else{
+		$password='';
+	}
+
+	if(isset($_REQUEST['cpassword'])){
+		$cpassword=$_REQUEST['cpassword'];
+	}
+	else{
+		$cpassword='';
+	}
+	if(isset($_REQUEST['address'])){
+		$address=$_REQUEST['address'];
+	}
+	else{
+		$address='';
+	}
+
+    $sql = mysqli_query($link,"select * from student_info where contact=".$contact);
+	$result = mysqli_fetch_array($sql);
+	
+	if($result>0){
+
+		echo "2";
+
+	}else if($contact!=''){
+
+		$query1 = mysqli_query($link,"INSERT INTO `student_info` (`studuni_id`,`sname`,`semail`,`contact`,`password`,`address`) VALUES ('".$studid."','".$sname."','".$semail."','".$contact."','".$password."','".$address."')");
+		
+		echo "1";
+	}else{
+		 echo "0";
+	    	
+	}
+	
+}
+	
+function addproduct(){
+	global $link;
+	global $web;
+	global $web1;
+	
+	$date=date("d-m-Y h:m A");
+	
+	if(isset($_REQUEST['brand'])){
+		$brand=$_REQUEST['brand'];
+	}else{
+		$brand='';
+	}
+	if(isset($_REQUEST['proname'])){
+		$proname=$_REQUEST['proname'];
+	}else{
+		$proname='';
+	}
+	if(isset($_REQUEST['price'])){
+		$price=$_REQUEST['price'];
+	}else{
+		$price='';
+	}
+	if(isset($_REQUEST['stype'])){
+		$stype=$_REQUEST['stype'];
+	}
+	else{
+		$stype='';
+	}
+	if(isset($_REQUEST['weight'])){
+		$weight=$_REQUEST['weight'];
+	}
+	else{
+		$weight='';
+	}
+	if(isset($_REQUEST['qty'])){
+		$qty=$_REQUEST['qty'];
+	}
+	else{
+		$qty='';
+	}
+	if(isset($_REQUEST['pdescription'])){
+		$pdescription=$_REQUEST['pdescription'];
+	}
+	else{
+		$pdescription='';
+	}
+	
+	$stid = $_SESSION['mjwadmin_id'];
+	//$stid = $_SESSION['mjwadmin_id'];
+
+	if(isset($_SESSION['mjwadmin_id'])){
+		$query1 = mysqli_query($link,"INSERT INTO `products` (`brandname`,`pname`,`price`,`stype`,`description`,`weight`,`qty`,`studid`) VALUES ('".$brand."','".$proname."','".$price."','".$stype."','".$pdescription."','".$weight."','".$qty."','".$stid."')");
+	
+	
+	if($query1){
+		$id=mysqli_insert_id($link);
+
+		if(isset($_FILES['homeimage']['name']) && $_FILES['homeimage']['name'] != '')
+		{
+			$file_count = count((array)$_FILES['homeimage']['tmp_name']);
+			if($file_count > 0)
+			{
+				$errors= array();
+				$number=rand();
+				$file_name = $_FILES['homeimage']['name'];
+				$file_size =$_FILES['homeimage']['size'];
+				$file_tmp =$_FILES['homeimage']['tmp_name'];
+				$file_type=$_FILES['homeimage']['type'];	
+				$desired_dir="../brand-photos/";
+				$ext = pathinfo($file_name, PATHINFO_EXTENSION);
+				$preg=array("'"," ",".");
+				$file = str_replace($preg,"_",$file_name)."_".$number.".".$ext;
+				//$file = $number.".".$ext;
+				if(move_uploaded_file($file_tmp,$desired_dir.$file))
+					{
+						$$bchk=mysqli_query($link,"select * from products where pid='".$_REQUEST['pid']."'");
+						$bchkdata=mysqli_fetch_array($bchk);
+
+							
+						if(mysqli_num_rows($bchk)>0){
+							$query=mysqli_query($link,"update products set `path`='".$file."' where pid='".$_REQUEST['pid']."'");
+							
+						}else{
+							$query=mysqli_query($link,"update products set `path`='".$file."' where pid='".$id."'");
+						}
+					}
+				
+			}
+		}
+	}
+		
+	}
+	if($query1){
+		echo "1";
+	}else{
+		echo "0";
+	}
+}
+
 function product_listing(){
 	global $link;
 	global $web;
@@ -206,18 +434,21 @@ function addcontact(){
 		$message='';
 	}
 	
-       $sql = mysqli_query($link,"select * from contact");
+    $sql = mysqli_query($link,"select * from contact where contact=".$contact);
 	$result = mysqli_fetch_array($sql);
 	
-	if($result['contact']==$contact){
+	if($result>0){
+
 		echo "2";
-	}else{
+
+	}else if($contact!=''){
+
 		$query1 = mysqli_query($link,"INSERT INTO `contact` (`name`,`email`,`contact`,`message`) VALUES ('".$name."','".$email."','".$contact."','".$message."')");
-             if($query1){
+
 		echo "1";
-	     }else{
+	}else{
 		 echo "0";
-	     }	
+	    	
 	}
 	
 	
